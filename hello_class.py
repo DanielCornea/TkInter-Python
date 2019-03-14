7cf9d91ac0d49c21971c4ab1687dc7c22e3b0668
Daniel Cornea <danok.cornea@gmail.com>
2019-03-14 18:03:14 +0100

    created the the panned window and added first child

--- hello_class.py
+++ hello_class.py
@@ -4,34 +4,57 @@ class App:
     def __init__(self, master): 
         # the main frame 
         frame = Frame(master)
-        frame.pack()
+        frame.pack(padx=2, pady=2)
 
         # panned window 
-        m = PanedWindow(orient = HORIZONTAL)
-        m.pack(fill = BOTH, expand = 1)
+        m = PanedWindow(orient = VERTICAL)
+        m.pack(fill = BOTH, expand = 5)
 
-        left = Label(m, text = "right pane")
-        m.add(left)
+        # first_pane = Label(m, bg = "green")
+        # first_pane.pack()
+        
+            
+        first_pane = Label(m, text = "first_pane", bg = "green")
+        m.add(first_pane)
 
-        right = Label(m, text = "right pane")
-        m.add(right)
+        
+        second_pane = Label(m)
+        m.add(second_pane)
+    
 
-        print("added panes")
+        third_pane = Label(m, text = "third_pane")
+        third_pane.pack()
 
-        self.button = Button(
-            left, text = "QUIT", fg = "red", command = frame.quit
-        )
-        self.button.pack(side = LEFT)
+        fourth_pane = Label(m, text = "fourth_pane")
+        fourth_pane.pack()
 
-        self.hi_there = Button(
-            right, text = "HELLO", command = self.say_hi
-        )
-        self.hi_there.pack()
+        fifth_pane = Label(m, text = "fifth_pane")
+        fifth_pane.pack()
 
-        print("butons added")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+
+    #     left = Label(m, text = "right pane")
+    #     m.add(left)
+
+    #     right = Label(m, text = "right pane")
+    #     m.add(right)
+
+    #     print("added panes")
+
+    #     self.button = Button(
+    #         left, text = "QUIT", fg = "red", command = frame.quit
+    #     )
+    #     self.button.pack(side = LEFT)
+
+    #     self.hi_there = Button(
+    #         right, text = "HELLO", command = self.say_hi
+    #     )
+    #     self.hi_there.pack()
+
+    #     print("butons added")
+
+    # def say_hi(self):
+    #     print("hi there, everyone!")
 
 root = Tk()
 
