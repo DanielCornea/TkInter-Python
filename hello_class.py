4b8f865fb2cabbd8c5fcdc77e91141f5386c4478
Daniel Cornea <danok.cornea@gmail.com>
2019-03-15 15:49:05 +0100

    addded the buttons

--- hello_class.py
+++ hello_class.py
@@ -4,32 +4,108 @@ class App:
     def __init__(self, master): 
         # the main frame 
         frame = Frame(master)
-        frame.pack(padx=2, pady=2)
+        frame.pack(fill = BOTH)
+
+        first_row = Label(frame, text="Red", bg="red", fg="white")
+        first_row.pack(fill=BOTH)
+  
+        second_row = Label(frame, text="Green", bg="green", fg="black")
+        second_row.pack(fill=BOTH)
+  
+        third_row = Label(frame, text="Blue", bg="blue", fg="white")
+        third_row.pack(fill=BOTH)
+
+        fourth_row = Label(frame, text="Yellow", bg="yellow", fg="white")
+        fourth_row.pack(fill=BOTH)
+
+        fifth_row = Label(frame, text="Yellow", bg="yellow", fg="white")
+        fifth_row.pack(fill = BOTH)
+
+
+        button_0 = Button(fifth_row, text = "0")
+        button_0.pack(side = LEFT)
+
+        button_equals = Button(fifth_row, text = "=")
+        button_equals.pack(side = LEFT)
+
+        button_1 = Button(fourth_row, text = "1")
+        button_1.pack( side = LEFT)
+
+        button_2 = Button(fourth_row, text = "2")
+        button_2.pack(side = LEFT)
+
+        button_3 = Button(fourth_row, text = "3")
+        button_3.pack(side = LEFT)
+
+        button_4 = Button(third_row, text = "4")
+        button_4.pack(side = LEFT)
+
+        button_5 = Button(third_row, text = "5")
+        button_5.pack(side = LEFT)
+
+        button_6 = Button(third_row, text = "6")
+        button_6.pack(side = LEFT)
+
+        button_7 = Button(second_row, text = "7")
+        button_7.pack(side = LEFT)
+
+        button_8 = Button(second_row, text = "8")
+        button_8.pack(side = LEFT)
+
+        button_9 = Button(second_row, text = "9")
+        button_9.pack(side = LEFT)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         # panned window 
-        m = PanedWindow(orient = VERTICAL)
-        m.pack(fill = BOTH, expand = 5)
+        # m = PanedWindow(orient = VERTICAL)
+        # m.pack(fill = BOTH, expand = 5)
+
+
+
+
+
+
+
+
+
+
 
         # first_pane = Label(m, bg = "green")
         # first_pane.pack()
         
             
-        first_pane = Label(m, text = "first_pane", bg = "green")
-        m.add(first_pane)
+        # first_pane = Label(m, text = "first_pane", bg = "green")
+        # m.add(first_pane)
 
         
-        second_pane = Label(m)
-        m.add(second_pane)
+        # second_pane = Label(m, text = "second_pane")
+        # m.add(second_pane)
     
 
-        third_pane = Label(m, text = "third_pane")
-        third_pane.pack()
+        # third_pane = Label(m, text = "third_pane")
+        # m.add(third_pane)
 
-        fourth_pane = Label(m, text = "fourth_pane")
-        fourth_pane.pack()
+        # fourth_pane = Label(m, text = "fourth_pane")
+        # m.add(fourth_pane)
 
-        fifth_pane = Label(m, text = "fifth_pane")
-        fifth_pane.pack()
+        # fifth_pane = Label(m, text = "fifth_pane", bg = "green")
+        # m.add(fifth_pane)
 
 
 
